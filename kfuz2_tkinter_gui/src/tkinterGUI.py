b7dbd8f156988bcb297d768f7e04a003a01afa3c
Shtoyan <shtoyan@proton.me>
2023-06-13 11:22:50 +0400

    Make tkinter gui fancier

--- kfuz2_tkinter_gui/src/tkinterGUI.py
+++ kfuz2_tkinter_gui/src/tkinterGUI.py
@@ -6,12 +6,14 @@
 import os
 import tkinter as tk
 from tkinter import ttk, filedialog
+from typing import Any
 from webbrowser import open_new
 from subprocess import run
 from pathlib import Path
 from os import walk
-from typing import Any
 from platform import uname
+
+unrealExtensions: tuple[str, ...] = (".u", ".utx", ".usx", ".ukx", ".uax", ".rom")
 
 
 class App(tk.Tk):
@@ -24,19 +26,23 @@ class App(tk.Tk):
         path_script: Path = Path(os.path.realpath(__file__))
         self.cli: Path = path_script.parent.joinpath(self.kfuz2)
         if not self.cli.exists():
-            print(f"Can not find {self.cli}")
+            print(f"Can not find {self.cli=}")
             exit()
 
         self.File_List: list[str] = []
         self.Input: str = ""
         self.Output: str = ""
 
-        self.geometry("500x300")
+        self.geometry("700x250")
         self.title("Killing Floor 1 Redirect Tool")
         self.add_Menus()
 
-        self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=3)
+        self.columnconfigure(0, weight=0)
+        self.columnconfigure(1, weight=1)
+        self.columnconfigure(2, weight=1)
+        self.columnconfigure(3, weight=1)
+        self.columnconfigure(4, weight=1)
+        self.columnconfigure(5, weight=1)
 
         self.create_widgets()
         self.mainloop()
@@ -49,6 +55,10 @@ class App(tk.Tk):
                 return "kfuz2"
 
     def run_cli(self, args: list[Any]) -> None:
+        if self.Output != "":
+            args.insert(0, "-o")
+            args.insert(0, self.Output)
+
         args.insert(0, self.cli)
 
         try:
@@ -57,29 +67,55 @@ class App(tk.Tk):
             print(str(e))
 
     def create_widgets(self) -> None:
-        lb_output = ttk.Label(self, text="Output:")
-        lb_input = ttk.Label(self, text="Input:")
+        lb_input = ttk.Label(self, text="Input: ...", width=80, background="lightgrey")
+        lb_output = ttk.Label(
+            self, text="Output: ...", width=80, background="lightgrey"
+        )
 
         btn_select_input = ttk.Button(
-            self, text="Select Input", command=lambda: self.select_input(lb_input)
+            self,
+            width=15,
+            text="Select Input",
+            command=lambda: self.select_input(lb_input, btn_Compress, btn_Uncompress),
         )
         btn_select_output = ttk.Button(
-            self, text="Select Output", command=lambda: self.select_output(lb_output)
+            self,
+            width=15,
+            text="Select Output",
+            command=lambda: self.select_output(lb_output, btn_open_output),
         )
-        btn_open_output = ttk.Button(self, text="Open Output", command=self.open_output)
-        btn_Compress = ttk.Button(self, text="Compress", command=self.start_Compression)
+        btn_open_output = ttk.Button(
+            self,
+            width=15,
+            text="Open Output",
+            state="disabled",
+            command=self.open_output,
+        )
+        btn_Compress = ttk.Button(
+            self,
+            width=30,
+            text="Compress",
+            state="disabled",
+            command=self.start_Compression,
+        )
         btn_Uncompress = ttk.Button(
-            self, text="Uncompress", command=self.start_Uncompression
+            self,
+            width=30,
+            text="Uncompress",
+            state="disabled",
+            command=self.start_Uncompression,
         )
 
-        lb_output.grid(column=0, row=0, columnspan=4, sticky=tk.S, padx=5, pady=5)
-        lb_input.grid(column=0, row=1, columnspan=4, sticky=tk.S, padx=5, pady=5)
-
-        btn_select_input.grid(column=0, row=2, sticky=tk.S, padx=5, pady=5)
-        btn_select_output.grid(column=1, row=2, sticky=tk.S, padx=5, pady=5)
-        btn_open_output.grid(column=2, row=2, sticky=tk.S, padx=5, pady=5)
-        btn_Compress.grid(column=3, row=2, sticky=tk.S, padx=5, pady=5)
-        btn_Uncompress.grid(column=4, row=2, sticky=tk.S, padx=5, pady=5)
+        # Grid
+        # Labels
+        lb_input.grid(column=1, row=0, columnspan=5, sticky=tk.EW, padx=5, pady=5)
+        lb_output.grid(column=1, row=1, columnspan=5, sticky=tk.EW, padx=5, pady=5)
+        # Buttons
+        btn_select_input.grid(column=0, row=0, padx=5, pady=5)
+        btn_select_output.grid(column=0, row=1, sticky=tk.S, padx=5, pady=5)
+        btn_open_output.grid(column=0, row=3, sticky=tk.S, padx=5, pady=5)
+        btn_Compress.grid(column=2, row=5, sticky=tk.S, padx=5, pady=5)
+        btn_Uncompress.grid(column=3, row=5, sticky=tk.S, padx=5, pady=5)
 
     def add_Menus(self) -> None:
         menu = tk.Menu(self)
@@ -99,13 +135,13 @@ class App(tk.Tk):
         self.refresh_file_list()
 
         for file in self.File_List:
-            self.run_cli([self.get_output_args() + file])
+            self.run_cli([file])
         print("=============== DONE ===============")
 
     def start_Uncompression(self) -> None:
         self.refresh_file_list()
         for file in self.File_List:
-            self.run_cli([self.get_output_args() + "-d", file])
+            self.run_cli(["-d", file])
         print("=============== DONE ===============")
 
     def refresh_file_list(self) -> None:
@@ -116,32 +152,32 @@ class App(tk.Tk):
         else:
             for root, _, files in walk(self.Input):
                 for file in files:
-                    # if (
-                    #     file.endswith(unrealExtensions)
-                    #     and file.lower() not in disallowedPackages
-                    # ):
-                    self.File_List.append(str(Path(root) / file))
+                    if file.endswith(unrealExtensions):
+                        self.File_List.append(str(Path(root) / file))
 
-    def get_output_args(self) -> str:
-        args: str = ""
-        if self.Output != "":
-            args = f"-o {self.Output} "
-        return args
-
-    def select_output(self, label: ttk.Label) -> str:
+    def select_output(self, label: ttk.Label, button: ttk.Button) -> str:
         self.Output = filedialog.askdirectory(title="Select Output Folder")
-        label.config(text=self.Output)
+        if self.Output != "":
+            label.config(text=self.Output)
+            label.config(background="lightgreen")
+            button.config(state="enabled")
         return self.Output
 
-    def select_input(self, label: ttk.Label) -> str:
+    def select_input(
+        self, label: ttk.Label, btn_compress: ttk.Button, btn_uncompress: ttk.Button
+    ) -> str:
         self.Input = filedialog.askdirectory(title="Select Input Folder")
-        label.config(text=self.Input)
+        if self.Input != "":
+            label.config(text=self.Input)
+            label.config(background="lightgreen")
+            btn_compress.config(state="enabled")
+            btn_uncompress.config(state="enabled")
         return self.Input
 
     def open_output(self) -> None:
         path_output = Path(self.Output)
         if not path_output.exists():
-            print(f"Can not find {path_output}!")
+            print(f"Can not find {path_output=}!")
 
         match self.current_system:
             case "Darwin":
