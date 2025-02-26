0e760d705e012d4d3b461fbcff3d85cd4f97460f
Shtoyan <shtoyan@proton.me>
2025-02-26 18:27:19 +0400

    Start of a server crate

---
+++ crates/kfuz2_server/src/lib.rs
@@ -0,0 +1,15 @@
+// pub mod cache;
+pub mod config;
+pub mod server;
+
+#[derive(thiserror::Error, Debug)]
+pub enum ServerErrors {
+    #[error(transparent)]
+    IOError(#[from] std::io::Error),
+    #[error(transparent)]
+    HyperError(#[from] hyper::Error),
+    #[error(transparent)]
+    TomlSerError(#[from] toml::ser::Error),
+    #[error(transparent)]
+    TomlDeError(#[from] toml::de::Error),
+}
